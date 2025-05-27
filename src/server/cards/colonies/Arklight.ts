54d7f38ec7962ce2d79a654c20f1576b91a170e6
wjcxymy <43081820+wjcxymy@users.noreply.github.com>
2025-05-27 22:33:34 +0800

    feat: 添加公司卡牌 RB-CORP-01 - Arklight Rebalanced
    - 类型：公司卡牌，起始资金 45 M€，标签为动物（Animal）
    - 公司能力：
      - 当你打出动物或植物标签时，增加1个动物资源到此卡
      - 新增效果：当在任意卡上获得动物资源时，获得等量 M€
    - 实现了 onResourceAdded 钩子，支持“获得动物资源时获得1 M€”效果
    - 添加了卡牌渲染（LOGO、说明文本）及中英文本地化支持
    - 更新卡牌注册，替换旧版 Arklight 为 Rebalanced 版本

--- src/server/cards/colonies/Arklight.ts
+++ src/server/cards/colonies/Arklight.ts
@@ -8,9 +8,24 @@ import {CardName} from '../../../common/cards/CardName';
 import {CardRenderer} from '../render/CardRenderer';
 
 export class Arklight extends CorporationCard {
-  constructor() {
+  constructor({
+    name = CardName.ARKLIGHT,
+    metadata = {
+      cardNumber: 'R04',
+      description: 'You start with 45 M€. Increase your M€ production 2 steps. 1 VP per 2 animals on this card.',
+      renderData: CardRenderer.builder((b) => {
+        b.megacredits(45).nbsp.production((pb) => pb.megacredits(2));
+        b.corpBox('effect', (ce) => {
+          ce.effect('When you play an animal or plant tag, including this, add 1 animal to this card.', (eb) => {
+            eb.tag(Tag.ANIMAL).slash().tag(Tag.PLANT).startEffect.resource(CardResource.ANIMAL);
+          });
+          ce.vSpace(); // to offset the description to the top a bit so it can be readable
+        });
+      }),
+    },
+  } = {}) {
     super({
-      name: CardName.ARKLIGHT,
+      name,
       tags: [Tag.ANIMAL],
       startingMegaCredits: 45,
       resourceType: CardResource.ANIMAL,
@@ -21,19 +36,7 @@ export class Arklight extends CorporationCard {
         addResources: 1,
       },
 
-      metadata: {
-        cardNumber: 'R04',
-        description: 'You start with 45 M€. Increase your M€ production 2 steps. 1 VP per 2 animals on this card.',
-        renderData: CardRenderer.builder((b) => {
-          b.megacredits(45).nbsp.production((pb) => pb.megacredits(2));
-          b.corpBox('effect', (ce) => {
-            ce.effect('When you play an animal or plant tag, including this, add 1 animal to this card.', (eb) => {
-              eb.tag(Tag.ANIMAL).slash().tag(Tag.PLANT).startEffect.resource(CardResource.ANIMAL);
-            });
-            ce.vSpace(); // to offset the description to the top a bit so it can be readable
-          });
-        }),
-      },
+      metadata,
     });
   }
 
@@ -42,7 +45,7 @@ export class Arklight extends CorporationCard {
   }
 
   public onCardPlayed(player: IPlayer, card: ICard): void {
-    if (player.isCorporation(CardName.ARKLIGHT)) {
+    if (player.isCorporation(this.name)) {
       player.addResourceTo(this, {qty: card.tags.filter((cardTag) => cardTag === Tag.ANIMAL || cardTag === Tag.PLANT).length, log: true});
     }
   }
---
+++ src/server/cards/rebalanced/ArklightRebalanced.ts
@@ -0,0 +1,39 @@
+import {CardResource} from '../../../common/CardResource';
+import {CardName} from '../../../common/cards/CardName';
+import {CardRenderer} from '../render/CardRenderer';
+import {Tag} from '../../../common/cards/Tag';
+import {Arklight} from '../colonies/Arklight';
+import {IPlayer} from '../../../server/IPlayer';
+import {ICard} from '../ICard';
+import {Resource} from '../../../common/Resource';
+import {Size} from '../../../common/cards/render/Size';
+
+export class ArklightRebalanced extends Arklight {
+  constructor() {
+    super({
+      name: CardName.ARKLIGHT_REBALANCED,
+      metadata: {
+        cardNumber: 'RB-CORP-01',
+        description: 'You start with 45 M€. Increase your M€ production 2 steps. 1 VP per 2 animals on this card.',
+        renderData: CardRenderer.builder((b) => {
+          b.megacredits(45).nbsp.production((pb) => pb.megacredits(2));
+          b.corpBox('effect', (ce) => {
+            ce.vSpace(Size.LARGE);
+            ce.effect('When you play an animal or plant tag, including this, add 1 animal to this card.', (eb) => {
+              eb.tag(Tag.ANIMAL).slash().tag(Tag.PLANT).startEffect.resource(CardResource.ANIMAL);
+            });
+            ce.effect('When you gain an animal to ANY CARD, gain 1 M€.', (eb) => {
+              eb.resource(CardResource.ANIMAL).asterix().startEffect.megacredits(1);
+            });
+          });
+        }),
+      },
+    });
+  }
+
+  public onResourceAdded(player: IPlayer, card: ICard, count: number) {
+    if (card.resourceType === CardResource.ANIMAL || card.resourceType === CardResource.MONKEY) {
+      player.stock.add(Resource.MEGACREDITS, count, {log: true});
+    }
+  }
+}
