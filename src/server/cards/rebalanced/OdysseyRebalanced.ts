815ed84c175891ac9fa2f8caf8fc56bcccce7c92
wjcxymy <43081820+wjcxymy@users.noreply.github.com>
2025-06-14 08:24:46 +0800

    feat: 添加公司卡牌 RB-CORP-06 - Odyssey Rebalanced
    - 基于原版公司 Odyssey 进行重制，改动内容如下：
    
    - 卡牌效果改动
      - 将“支付费用重新打出一张事件卡”改为“免费重新打出一张事件卡”
    
    - 支持该重制卡的显示样式、卡名枚举、语言本地化与图标逻辑处理

---
+++ src/server/cards/rebalanced/OdysseyRebalanced.ts
@@ -0,0 +1,79 @@
+import {Tag} from '../../../common/cards/Tag';
+import {IPlayer} from '../../IPlayer';
+import {CardName} from '../../../common/cards/CardName';
+import {CardType} from '../../../common/cards/CardType';
+import {CardRenderer} from '../render/CardRenderer';
+import {Size} from '../../../common/cards/render/Size';
+import {CorporationCard} from '../corporation/CorporationCard';
+import {IActionCard} from '../ICard';
+import {SelectCard} from '../../../server/inputs/SelectCard';
+
+export class OdysseyRebalanced extends CorporationCard implements IActionCard {
+  constructor() {
+    super({
+      name: CardName.ODYSSEY_REBALANCED,
+      startingMegaCredits: 33,
+
+      metadata: {
+        cardNumber: 'RB-CORP-06',
+        description: 'You start with 33 M€.',
+        renderData: CardRenderer.builder((b) => {
+          b.megacredits(33);
+          b.corpBox('action', (ce) => {
+            ce.vSpace(Size.LARGE);
+            ce.effect('Your event cards stay face up, and their tags are in use as if those were automated (green) cards.', (eb) => {
+              eb.empty().startEffect.cards(1, {secondaryTag: Tag.EVENT}).text('up').asterix();
+            });
+            ce.action('Play for free an event card you have already played that has a base cost of 16M€ or less (INCLUDING events that place special tiles,) after which discard that card.', (eb) => {
+              eb.empty().startAction.tag(Tag.EVENT).asterix().nbsp.text('≤').nbsp.megacredits(16);
+            });
+          });
+        }),
+      },
+    });
+  }
+
+  // For Project Inspection
+  private checkLoops: number = 0;
+
+  public getCheckLoops(): number {
+    return this.checkLoops;
+  }
+
+  public availableEventCards(player: IPlayer) {
+    this.checkLoops++;
+    try {
+      const array = [];
+      for (const card of player.playedCards) {
+        // Special case Price Wars, which is not easy to work with.
+        if (card.name === CardName.PRICE_WARS) {
+          continue;
+        }
+        if (card.type === CardType.EVENT && card.cost <= 16) {
+          const details = card.canPlayPostRequirements(player);
+          if (details !== false) {
+            array.push({card, details});
+          }
+        }
+      }
+      return array;
+    } finally {
+      this.checkLoops--;
+    }
+  }
+
+  public canAct(player: IPlayer) {
+    return this.availableEventCards(player).length > 0;
+  }
+
+  public action(player: IPlayer) {
+    const eventCards = this.availableEventCards(player);
+    return new SelectCard('选择1张事件牌免费打出', '打出', eventCards.map((e) => e.card), {min: 1, max: 1})
+      .andThen((cards) => {
+        const card = cards[0];
+        player.removedFromPlayCards.push(card);
+        player.playCard(card, undefined, 'discard');
+        return undefined;
+      });
+  }
+}
